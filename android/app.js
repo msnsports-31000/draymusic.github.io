5e3f9cd2a3b659e316afcf1558da13df1413f0fd
draydenthemiiyt-maker <draydenthemiiyt@gmail.com>
2026-04-21 13:46:08 +0100

    Update app.js

--- android/app.js
+++ android/app.js
@@ -283,7 +283,7 @@ function playSong(index) {
     if (titleEl) titleEl.innerText = song.title; // innerText is better for old IE than textContent
     if (artistEl) artistEl.innerText = song.artist;
     if (artEl) artEl.src = song.art;
-    if (playBtn) playBtn.innerHTML = '<span class="material-symbols-rounded">pause</span>';
+    if (playBtn) playBtn.innerHTML = '<span class="win-icon-pack"></span>';
 }
 
 function playNext() { playSong(currentIndex + 1); }
@@ -294,10 +294,10 @@ if (btnPlayPause && audio) {
     btnPlayPause.onclick = function () {
         if (audio.paused) {
             safePlay(audio);
-            btnPlayPause.innerHTML = '<span class="material-symbols-rounded">pause</span>';
+            btnPlayPause.innerHTML = '<span class="win-icon-pack"></span>';
         } else {
             audio.pause();
-            btnPlayPause.innerHTML = '<span class="material-symbols-rounded">play_arrow</span>';
+            btnPlayPause.innerHTML = '<span class="win-icon-pack"></span>';
         }
     };
 }
