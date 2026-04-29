12c017d0df33e018c1d4761d692ad994001a1b44
draydenthemiiyt-maker <draydenthemiiyt@gmail.com>
2026-04-29 19:13:31 +0100

    Refactor apps.js for improved readability and structure

--- 10Store/apps.js
+++ 10Store/apps.js
@@ -91,7 +91,7 @@
                 '</div>';
 
             wrapper.onclick = (function (appId) {
-                return function () { window.location.href = `${pagehosturi}/app.html?id=${encodeURIComponent(appId)}`;
+                return function () { window.location.href = 'app.html?id=' + appId; };
             })(id);
 
             wrapper.appendChild(card);
